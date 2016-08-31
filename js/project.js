68842a833539559ecb97d0764ab10c2e70504fb4
abigailmuir <abby@motivatedesign.com>
2016-08-31 10:36:26 -0400

    Update

---
+++ js/project.js
@@ -0,0 +1,14 @@
+
+
+
+// $("document").ready(function(){
+
+// $("apology").appendto("body");
+
+// Sorry that I didn't include javascript :( 
+// I am a failure and my job has completely consumed all of my weekends 
+// and free time for the last month. 
+
+
+
+// });
